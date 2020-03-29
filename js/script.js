daba04c4377983caf2fa1483e366eb3ea3fd45ad
maxfinnsjo <max.finnsjo@gmail.com>
2020-03-29 14:02:14 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Menu script
+function toggleClassMenu() {
+	myMenu.classList.add("menu--animatable");	
+	myMenu.classList.toggle("menu--visible");
+}
+
+function OnTransitionEnd() {
+	myMenu.classList.remove("menu--animatable");
+}
+
+var myMenu = document.querySelector(".menu");
+var oppMenu = document.querySelector(".menu-icon");
+myMenu.addEventListener("transitionend", OnTransitionEnd, false);
+oppMenu.addEventListener("click", toggleClassMenu, false);
+myMenu.addEventListener("click", toggleClassMenu, false);
+//!Menu script
